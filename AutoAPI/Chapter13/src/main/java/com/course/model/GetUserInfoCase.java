f07805c92e5c12b92e6118fa1192d59c4a9eec6f
qym00725@163.com <qym00725@163.com>
2019-09-19 13:52:37 +0800

    删除多余代码

--- AutoAPI/Chapter13/src/main/java/com/course/model/GetUserInfoCase.java
+++
@@ -1,9 +0,0 @@
-package com.course.model;
-
-import lombok.Data;
-
-@Data
-public class GetUserInfoCase {
-    private String userId;
-    private String expected;
-}
--- AutoAPI/Chapter15/src/main/java/com/course/Application.java
+++
@@ -1,9 +0,0 @@
-package com.course;
-
-import org.springframework.boot.SpringApplication;
-
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
-    }
-}
